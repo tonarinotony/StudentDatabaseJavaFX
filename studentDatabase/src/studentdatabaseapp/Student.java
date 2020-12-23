6f8709d56028257bbb1df9351e4b39a2222ba22b
tonyhuang <tonarinotony@gmail.com>
2020-12-22 19:37:37 -0500

    adding student database study

---
+++ studentDatabase/src/studentdatabaseapp/Student.java
@@ -0,0 +1,24 @@
+package studentdatabaseapp;
+
+import java.util.Scanner;
+
+public class Student {
+	private String firstName, lastName;
+	private String year;
+	private int studentID;
+	private String courses;
+	private String tuitionBalance;
+	private int costOfCourse = 600;
+	
+	public Student(){
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter student first name: ");
+		
+		System.out.print("Enter student last name");
+	}
+	//generate id
+	// enroll in courses
+	// view balance
+	//pay tuition
+	//show status
+}
---
+++ studentDatabase/src/studentdatabaseapp/StudentDataBaseApp.java
@@ -0,0 +1,10 @@
+package studentdatabaseapp;
+
+public class StudentDataBaseApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
